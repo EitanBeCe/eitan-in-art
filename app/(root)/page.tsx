04fe3fc880c0dae22c980a55eccc1addb35b7cf8
Eitan Elberg <eitan.elberg@consumrz.com>
2025-06-30 15:54:25 +0300

    grid

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,19 @@
 export default function Home() {
-  return (
-    <h1 className="text-2xl">Eitan in Art</h1>
-  );
+    return (
+        <>
+            <h1 className="text-2xl">Eitan in Art</h1>
+
+            <section className="section_container">
+                <ul className="mt-7 card_grid">
+                    {paintings?.length > 0 ? (
+                        paintings.map((p: PaintingCodable) => (
+                            <PaintingCard key={p?._id} post={p}/>
+                        ))
+                    ) : (
+                        <p className="no-results">No startups found</p>
+                    )}
+                </ul>
+            </section>
+        </>
+    );
 }
