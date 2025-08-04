5236b66ba530cbb23ee0b33f36b5dcb1bd23170e
Eitan Elberg <eitan.elberg@consumrz.com>
2025-08-04 17:09:13 +0300

    shadows

--- components/PaintingCardRowView.tsx
+++ components/PaintingCardRowView.tsx
@@ -8,22 +8,24 @@ export function PaintingCardRowView({ slug, title, description, images }: Painti
 ) {
     return (
         <a href={`/${slug}`} target="_blank">
-            <CardContainer className="inter-var" containerClassName="py-8 flex items-center justify-center">
+            <CardContainer className="inter-var" containerClassName="pt-8 pb-0 flex items-center justify-center">
                 {/*<CardBody className={`bg-gray-50 relative group/card hover:shadow-[0_0_45px_0px] hover:${shadowColor} dark:hover:${shadowColor}/80 dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border transition-shadow duration-200 ease-linear`}>*/}
                 <CardBody
                     className={clsx(
                         "bg-gray-50 relative group/card hover:shadow-[0_0_45px_0px]",
                         "dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border transition-shadow duration-200 ease-linear",
-                        slug.includes("chail")
+                        slug.includes("chail") || slug.includes("mizmor")
                             ? "hover:shadow-pink-400"
                             :slug.includes("chai")
                                 ? "hover:shadow-red-500"
-                                : slug.includes("gold")
+                                : slug.includes("gold") || slug.includes("spring")
                                     ? "hover:shadow-yellow-400"
-                                    : slug.includes("blue") || slug.includes("hillel")
+                                    : slug.includes("blue") || slug.includes("hillel") || slug.includes("wedding") || slug.includes("talit")
                                         ? "hover:shadow-sky-400"
                                             : slug.includes("white")
                                                 ? "hover:shadow-white-100"
+                                                    : slug.includes("russian") || slug.includes("horse") || slug.includes("ketubah") || slug.includes("savor")
+                                                        ? "hover:shadow-green-500"
                         : "hover:shadow-purple-500 dark:hover:shadow-purple-500/80",
                     )}
                 >
